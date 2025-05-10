256bfbfbfccf6b602db222a940ab62cd717517cf
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-10 21:09:01 +0530

    Create MoveAl ZeroestoEnd.cpp

---
+++ MICROSOFT/MoveAl ZeroestoEnd.cpp
@@ -0,0 +1,22 @@
+// GFG: Move All Zeroes to End
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int l,r;
+        l=r=0;
+        while(r<n)
+        {
+            if(arr[r]!=0)
+            {
+                swap(arr[l],arr[r]);
+                l++;
+            }
+            r++;
+        }
+    }
+};
+// Time complexity:O(n)
+// space complexity:O(1)
