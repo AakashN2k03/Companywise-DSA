47450462a57babc8c0c55ea0aa5faaf2f3b6c660
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-10 21:21:41 +0530

    Create Rotateby90degree.cpp

---
+++ MICROSOFT/Rotateby90degree.cpp
@@ -0,0 +1,26 @@
+// GFG:Rotate by 90 degree
+
+class Solution {
+  public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>>& mat) {
+        // code here
+        int row=mat.size();
+        int col=mat[0].size();
+        
+        for(int i=0;i<row;i++)
+        {
+            reverse(mat[i].begin(),mat[i].end());
+        }
+        
+        for(int i=0;i<row;i++)
+        {
+            for(int j=i+1;j<col;j++)
+            {
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+    }
+};
+// Time Complexity: O(n^2) 
+// Space Complexity: O(1) 
