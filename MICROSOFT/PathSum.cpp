3e92d43d1d1255ca888edfcf35f43610b3f04a46
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-21 23:16:21 +0530

    Create PathSum.cpp

---
+++ MICROSOFT/PathSum.cpp
@@ -0,0 +1,20 @@
+// LEETCODE: 112. Path Sum
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+
+        if(!root->left && !root->right) // check leaf nodes
+        {
+            return targetSum==root->val;
+        }
+        // Recur for left and right subtree with updated sum
+        return hasPathSum(root->left,targetSum-root->val) ||
+         hasPathSum(root->right,targetSum-root->val);
+        
+    }
+};
+
+// Time Complexity:O(N)
+// Space Complexity:O(H) recursion stack where H = height of tree.
