6cc071f3c80c0f394f20a8e1eaad2459a5e7f372
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-28 16:47:31 +0530

    Create K-thsmallestElementInBST.cpp

---
+++ FLIPKART/K-thsmallestElementInBST.cpp
@@ -0,0 +1,28 @@
+// CODE 360:  Find K-th smallest Element in BST
+
+// Pattern:BST Specific Problems
+
+void inorder(BinaryTreeNode<int>* root,int k,int& small,int& count)
+{
+    if(!root) return;
+    inorder(root->left,k,small,count);
+    count++;
+
+    if(count==k) 
+    {
+      small=root->data;
+     // return  may or may not use return ,still both works
+    }
+
+    inorder(root->right,k,small,count);
+   
+}
+int kthSmallest(BinaryTreeNode<int>* root, int k) {
+    int small=-1;
+    int count=0;
+    inorder(root,k,small,count);
+    return small;
+}
+
+Time Complexity: O(n) (without return) , ( with return ) O(k)
+Space Complexity:O(h)
