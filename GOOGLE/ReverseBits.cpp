8f341a5f04c88aa18ac4d624670c1f63d4f038ad
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-11 15:23:04 +0530

    Update ReverseBits.cpp

--- GOOGLE/ReverseBits.cpp
+++ GOOGLE/ReverseBits.cpp
@@ -8,7 +8,7 @@ public:
         uint32_t res=0;
         for(int i=0;i<32;i++)
         {
-            res=res<<1;
+            res=res<<1; 
             res|=(n&1);
             n=n>>1;
         }
@@ -18,3 +18,8 @@ public:
 };
 // Time Complexity: O(1)
 // Space Complexity: O(1)
+
+// INTUITION:
+// res <<= 1 – Shift res left to make space for the next bit.
+// res |= (n & 1) – Extract the last bit of n and insert it into res.
+// n >>= 1 – Move to the next bit of n by shifting it right.
