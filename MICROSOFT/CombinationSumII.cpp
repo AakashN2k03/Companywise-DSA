5386911df412b61427352b3673491dd20d28cb12
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-12 14:20:09 +0530

    Update CombinationSumII.cpp

--- MICROSOFT/CombinationSumII.cpp
+++ MICROSOFT/CombinationSumII.cpp
@@ -4,32 +4,28 @@
 
 class Solution {
 public:
-    void comb2(int index,vector<int>& candidates, int target,vector<int>& temp, vector<vector<int>>&ans)
+void fn(int index,vector<int>& candidates, int target,vector<int>& temp,vector<vector<int>>&ans)
     {
-        if(target==0) 
+        if(target==0)
         {
             ans.push_back(temp);
             return;
         }
-        if(index==candidates.size()) return;
 
-        for(int i=index;i<candidates.size();i++)
-        {   if( i > index && candidates[i]==candidates[i-1]) continue;//skip duplicates
-            
-             if(candidates[i]<=target)
-            {
-            temp.push_back(candidates[i]);//pick
-            comb2(i+1,candidates,target-candidates[i],temp,ans);
-            temp.pop_back();//unpick
-            }
+       for (int i = index; i < candidates.size(); i++) {
+        if(i>index && candidates[i]==candidates[i-1]) continue;
+        if (candidates[i] <= target) {
+            temp.push_back(candidates[i]);
+            fn(i+1, candidates, target - candidates[i], temp, ans); 
+            temp.pop_back();
         }
-
     }
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>temp;
         sort(candidates.begin(),candidates.end());
-        comb2(0,candidates,target,temp,ans);
+        fn(0,candidates,target,temp,ans);
         return ans;
         
     }
