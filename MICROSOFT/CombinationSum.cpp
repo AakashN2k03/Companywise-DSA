7f3f45b69978b1c31988db3be387ad6d9b25c9ed
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-12 14:09:43 +0530

    Update CombinationSum.cpp

--- MICROSOFT/CombinationSum.cpp
+++ MICROSOFT/CombinationSum.cpp
@@ -4,33 +4,29 @@
 
 class Solution {
 public:
-    void comb(int index,vector<int>& candidates, int target,vector<int>& temp,vector<vector<int>>&ans)
+    void fn(int index,vector<int>& candidates, int target,vector<int>& temp,vector<vector<int>>&ans)
     {
         if(target==0)
         {
             ans.push_back(temp);
             return;
         }
-        if(index==candidates.size()) return;
 
-        //pick;
-        if(candidates[index]<=target)
-        {
-            temp.push_back(candidates[index]);
-            comb(index,candidates,target-candidates[index],temp,ans);
+       for (int i = index; i < candidates.size(); i++) {
+        if (candidates[i] <= target) {
+            temp.push_back(candidates[i]);
+            fn(i, candidates, target - candidates[i], temp, ans); // not i + 1, because we can reuse
             temp.pop_back();
         }
-
-        //not pick;
-        comb(index+1,candidates,target,temp,ans);
     }
+}
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>temp;
-        comb(0,candidates,target,temp,ans);
+        fn(0,candidates,target,temp,ans);
         return ans;
     }
 };
-
 // Time Complexity:O(2^target)
 // Space Complexity:O(N) (stack space)
