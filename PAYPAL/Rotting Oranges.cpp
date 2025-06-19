38b9c494b6d2863767b3094a4dc6b96a304436d3
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-19 21:51:15 +0530

    Update Rotting Oranges.cpp

--- PAYPAL/Rotting Oranges.cpp
+++ PAYPAL/Rotting Oranges.cpp
@@ -7,26 +7,25 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int vis[n][m];
+        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>> qu; // ROW , COL, TIME   
         int fresh_orange_count=0;
-        for(int i=0;i<n;i++)
+       for(int i=0;i<n;i++)
+     {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<m;j++)
+            if(grid[i][j]==2)
             {
-                if(grid[i][j]==2)
-                {
-                  qu.push({{i,j},0});
-                  vis[i][j]=2;
-                }
-                else{
-                    vis[i][j]=0;
-                }
-
-                if(grid[i][j]==1) fresh_orange_count++;
-                
+                qu.push({{i,j},0});
+                vis[i][j]=2;
             }
+            else if(grid[i][j]==1){
+                vis[i][j]=1;
+                fresh_orange_count++;
+            }
+        
         }
+    }
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
         int time=0;
@@ -44,7 +43,7 @@ public:
                 int neighbour_row=row+drow[i];
                 int neighbour_col=col+dcol[i];
 
-                if(neighbour_row>=0 && neighbour_row<n && neighbour_col>=0 && neighbour_col<m && vis[neighbour_row][neighbour_col]==0 && grid[neighbour_row][neighbour_col]==1)
+                if(neighbour_row>=0 && neighbour_row<n && neighbour_col>=0 && neighbour_col<m && vis[neighbour_row][neighbour_col]!=2 && grid[neighbour_row][neighbour_col]==1)
                 {
                     qu.push({{neighbour_row,neighbour_col},t+1});
                     vis[neighbour_row][neighbour_col]=2;
